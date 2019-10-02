515477a89fc972886b10706526820bf092797ba1
SaroshKhawar496 <sarosh.496@gmail.com>
2019-10-02 00:58:31 -0400

    Created ProjectSummary component to show each projects detail

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,30 +1,14 @@
-import React from 'react';
+import React from "react";
+import ProjectSummary from "./ProjectSummary";
 
 const ProjectList = () => {
-    return (
-        <div className="project-list section">
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by Sarosh Khawar</p>
-                    <p className="grey-text">10-October, 3pm</p>
-                </div>
-            </div>
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by Sarosh Khawar</p>
-                    <p className="grey-text">10-October, 3pm</p>
-                </div>
-            </div>
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by Sarosh Khawar</p>
-                    <p className="grey-text">10-October, 3pm</p>
-                </div>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="project-list section">
+      <ProjectSummary />
+      <ProjectSummary />
+      <ProjectSummary />
+      <ProjectSummary />
+    </div>
+  );
+};
 export default ProjectList;
