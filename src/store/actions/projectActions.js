d38809a02c98f4ec9f89e8c922371ecc2118602b
SaroshKhawar496 <sarosh.496@gmail.com>
2019-10-07 14:53:56 -0400

    Installed Thunk for async code. Connected CreateProject to store.

---
+++ src/store/actions/projectActions.js
@@ -0,0 +1,10 @@
+export const createProject = project => {
+  //returning a fxn when using thunk to halt dispatch and do async calls to DB and then call dispatch
+  return (dispatch, getState) => {
+    //make async call to DB then resume db
+    dispatch({
+      type: "CREATE_PROJECT",
+      project: project
+    });
+  };
+};
