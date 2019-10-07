afbb9408fabbf35046406dcc494e989e596d319a
SaroshKhawar496 <sarosh.496@gmail.com>
2019-10-07 15:43:08 -0400

    Added firestore, firebase store enhancers

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,6 +1,6 @@
 export const createProject = project => {
   //returning a fxn when using thunk to halt dispatch and do async calls to DB and then call dispatch
-  return (dispatch, getState) => {
+  return (dispatch, getState, { getFirebase, getFirestore }) => {
     //make async call to DB then resume db
     dispatch({
       type: "CREATE_PROJECT",
