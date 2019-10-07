6ad3bdc113f4e5cc37eb8805fbc124a08bf27f81
SaroshKhawar496 <sarosh.496@gmail.com>
2019-10-07 16:12:46 -0400

    Adding the project to firebase. not updating state yet

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -2,9 +2,27 @@ export const createProject = project => {
   //returning a fxn when using thunk to halt dispatch and do async calls to DB and then call dispatch
   return (dispatch, getState, { getFirebase, getFirestore }) => {
     //make async call to DB then resume db
-    dispatch({
-      type: "CREATE_PROJECT",
-      project: project
-    });
+    const firestore = getFirestore();
+    firestore
+      .collection("projects")
+      .add({
+        ...project,
+        authorFirstName: "Sarosh",
+        authorLastName: "Khawar",
+        authorId: 123,
+        createdAt: new Date()
+      })
+      .then(() => {
+        dispatch({
+          type: "CREATE_PROJECT",
+          project: project
+        });
+      })
+      .catch(err => {
+        dispatch({
+          type: "CREATE_PROJECT_ERROR",
+          err
+        });
+      });
   };
 };
