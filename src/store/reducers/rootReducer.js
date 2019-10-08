5a1d6e19408f4a70cade3f9f035a0de70fe13190
SaroshKhawar496 <sarosh.496@gmail.com>
2019-10-08 17:43:14 -0400

    Using old dependencies. Connected Dashboard to firestore to get projects from firebase

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -2,11 +2,13 @@ import authReducer from "./authReducer";
 import projectReducer from "./projectReducer";
 
 import { combineReducers } from "redux";
+import { firestoreReducer } from "redux-firestore";
 
 const rootReducer = combineReducers({
   auth: authReducer,
   //projects array inside the initState in projectReducer is now stored on the 'project'
-  project: projectReducer
+  project: projectReducer,
+  firestore: firestoreReducer
 });
 
 export default rootReducer;
