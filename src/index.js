6d7aea097bfdd285f76c9ca8a9386b2f6967fe13
SaroshKhawar496 <sarosh.496@gmail.com>
2019-10-08 23:24:19 -0400

    using firebaseAuthIsReady to render after auth is ready

--- src/index.js
+++ src/index.js
@@ -22,18 +22,22 @@ const store = createStore(
       })
     ),
     reduxFirestore(fbConfig),
-    reactReduxFirebase(fbConfig)
+    //attachAuthIsReady allows you to access property on store firebaseAuthIsReady
+    reactReduxFirebase(fbConfig, { attachAuthIsReady: true })
   )
 );
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
+//rendering the dom when the firebase is ready so you don't see the signout links on page refresh
+store.firebaseAuthIsReady.then(() => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById("root")
+  );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  // If you want your app to work offline and load faster, you can change
+  // unregister() to register() below. Note this comes with some pitfalls.
+  // Learn more about service workers: https://bit.ly/CRA-PWA
+  serviceWorker.unregister();
+});
