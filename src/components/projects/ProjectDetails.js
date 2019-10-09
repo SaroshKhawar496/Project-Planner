8e001cc280a71431a3081f51277bf096e63e9a1f
SaroshKhawar496 <sarosh.496@gmail.com>
2019-10-08 21:58:27 -0400

    Showing the actual project details when clicked on

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,27 +1,50 @@
 import React from "react";
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+import { compose } from "redux";
 
 //props coming from react-router
-export default function ProjectDetails(props) {
+const ProjectDetails = props => {
   //   console.log(props);
   //getting the project id from route params
-  const id = props.match.params.id;
-  return (
-    <div className="container section project-details">
-      <div className="card z-depth-0">
-        <div className="card-content">
-          <span className="card-title">Project Title - {id}</span>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum,
-            repellendus amet reprehenderit minima vitae ipsam ea, incidunt
-            blanditiis assumenda placeat adipisci quibusdam totam. Praesentium
-            repudiandae possimus dignissimos libero quaerat consequatur!
-          </p>
-        </div>
-        <div className="card-action grey lighten-4 grey-text">
-          <div>Posted by Sarosh Khawar</div>
-          <div>2nd Oct, 2am</div>
+  // const id = props.match.params.id;
+  const project = props.project;
+  if (project) {
+    return (
+      <div className="container section project-details">
+        <div className="card z-depth-0">
+          <div className="card-content">
+            <span className="card-title">{project.title}</span>
+            <p>{project.content}</p>
+          </div>
+          <div className="card-action grey lighten-4 grey-text">
+            <div>
+              Posted by {project.authorFirstName} {project.authorLastName}
+            </div>
+            <div>2nd Oct, 2am</div>
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  } else {
+    return (
+      <div className="container center">
+        <p>Loading project.....</p>
+      </div>
+    );
+  }
+};
+
+const mapStateToProps = (state, ownProps) => {
+  const id = ownProps.match.params.id;
+  const projects = state.firestore.data.projects;
+  const project = projects ? projects[id] : null;
+  return {
+    project: project
+  };
+};
+
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([{ collection: "projects" }])
+)(ProjectDetails);
--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ProjectSummary from "./ProjectSummary";
+import { Link } from "react-router-dom";
 
 const ProjectList = props => {
   const projects = props.projects;
@@ -10,7 +11,11 @@ const ProjectList = props => {
       component reaches out to db so need the following "And" condition */}
       {projects &&
         projects.map(project => {
-          return <ProjectSummary project={project} key={project.id} />;
+          return (
+            <Link to={"/project/" + project.id}>
+              <ProjectSummary project={project} key={project.id} />
+            </Link>
+          );
         })}
     </div>
   );
